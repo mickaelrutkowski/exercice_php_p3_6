2b0343153e4051be8a0f4a91c36873630d771393
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:30:04 +0100

    exercice_php_P3_6

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  $num = 20;
+  while ($num > 0){
+    echo ' ' . 'C\'est presque bon.';
+   $num --;
+  }
+ ?>
+</body>
+</html>
